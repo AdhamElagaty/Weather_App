7798556998e5c5333c18bf24383a349094019d21
Adham Elagaty <adhamalagaty@gmail.com>
2023-10-24 18:21:20 +0300

    Add assets image

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/views/search_view.dart';
 import 'package:weather_app/widgets/weather_status_widget.dart';
 
 class HomeView extends StatelessWidget {
