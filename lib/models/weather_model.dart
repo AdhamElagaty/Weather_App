ef694cb226a69097aaf844f04d0515b7347cda2b
Adham Elagaty <adhamalagaty@gmail.com>
2023-10-28 14:05:50 +0200

    create cubit/get_waether_cubit folder and craete weatherStates

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -1,16 +1,30 @@
 class WeatherModel {
   final String name;
-  final String lastUpdate;
-  final int temp;
-  final int maxTemp;
-  final int minTemp;
+  final String lastUpdateTime;
+  final double temp;
+  final double maxTemp;
+  final double minTemp;
   final String? image;
   final String condtion;
 
-  WeatherModel({required this.name, required this.lastUpdate, required this.temp, required this.maxTemp, required this.minTemp, required this.image, required this.condtion});
+  WeatherModel(
+      {required this.name,
+      required this.lastUpdateTime,
+      required this.temp,
+      required this.maxTemp,
+      required this.minTemp,
+      required this.image,
+      required this.condtion});
 
-  factory WeatherModel.fromjson(json){
-    return WeatherModel(name: name, lastUpdate: lastUpdate, temp: temp, maxTemp: maxTemp, minTemp: minTemp, image: image, condtion: condtion,)
+  factory WeatherModel.fromjson(json) {
+    return WeatherModel(
+        name: json["location"]["name"],
+        lastUpdateTime: json["current"]["last_updated"],
+        temp: json["current"]["temp_c"],
+        maxTemp: json["forecast"]["forecastday"][0]["day"]["maxtemp_c"],
+        minTemp: json["forecast"]["forecastday"][0]["day"]["mintemp_c"],
+        image: json["forecast"]["forecastday"][0]["day"]["condition"]["icon"],
+        condtion: json["forecast"]["forecastday"][0]["day"]["condition"]
+            ["text"]);
   }
-
 }
