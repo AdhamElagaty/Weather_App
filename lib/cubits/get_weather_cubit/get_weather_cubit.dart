5abf282a7cc46ef18cea3466273ffb8eb0fda6cd
Adham Elagaty <adhamalagaty@gmail.com>
2023-10-28 14:47:54 +0200

    add flutter_bloc package and create blockProvider

--- lib/cubits/get_weather_cubit/get_weather_cubit.dart
+++ lib/cubits/get_weather_cubit/get_weather_cubit.dart
@@ -1,5 +1,5 @@
-import 'package:bloc/bloc.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubits/get_weather_cubit/get_weather_state.dart';
 import 'package:weather_app/models/weather_model.dart';
 import 'package:weather_app/services/weather_service.dart';
