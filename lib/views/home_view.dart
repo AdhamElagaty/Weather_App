93bdeac5a372565849635949f3e6443568a6b04b
Adham Elagaty <adhamalagaty@gmail.com>
2023-10-28 14:57:09 +0200

    integrate WeatherCubit

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_state.dart';
 import 'package:weather_app/views/search_view.dart';
+import 'package:weather_app/widgets/no_weather_widget.dart';
 import 'package:weather_app/widgets/weather_status_widget.dart';
 
 class HomeView extends StatelessWidget {
@@ -26,7 +30,19 @@ class HomeView extends StatelessWidget {
           )
         ],
       ),
-      body: const WeatherStatusWidget(),
+      body: BlocBuilder<GetWeatherCubit, GetWeatherState>(
+        builder: (context, state) {
+          if (state is InitialState) {
+            return const NoWeatherWidget();
+          } else if (state is WeatherLoadedState) {
+            return const WeatherStatusWidget();
+          } else {
+            return const Center(
+              child: Text("oops there was an error, try later!"),
+            );
+          }
+        },
+      ),
     );
   }
 }
